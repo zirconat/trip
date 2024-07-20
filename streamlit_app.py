5fbeb1efb4959b85aed680c7f354f3e605dcba59
zirconat <168637627+zirconat@users.noreply.github.com>
2024-07-20 14:37:03 +0000

    changed description

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import pandas as pd
+
+
+st.set_page_config(
+    page_title="Trips & Visits",
+    page_icon=":airplane:"
+    layout="wide"
+)
 
 st.title(" ✈️ Trips & Visits")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Displays trips and visits for WY24/25"
 )
+
+#@st.cache_data
+#def load_data(path: str):
+  #  data = pd.read_excel(path)
+   # return data
+
+#df = load_data("./Trip and visit WY24_25 Database.xlsx")
+#st.dataframe(df)
