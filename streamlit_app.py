e5638c59420ef889eb1e2a5cd6267a318c90d3d6
zirconat <168637627+zirconat@users.noreply.github.com>
2024-07-29 07:37:02 +0000

    remove plotly

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+#import plotly.express as px
 import altair as alt
 from streamlit_option_menu import option_menu
 import streamlit_shadcn_ui as ui
